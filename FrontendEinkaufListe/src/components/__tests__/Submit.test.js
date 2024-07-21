ffd033012fcc29108ae9fc885d8f9ac7ed4dfb22
GeorgWenderoth <108282544+GeorgWenderoth@users.noreply.github.com>
2024-07-21 18:15:58 +0200

    Cleaned Submit.test.js

--- FrontendEinkaufListe/src/components/__tests__/Submit.test.js
+++ FrontendEinkaufListe/src/components/__tests__/Submit.test.js
@@ -2,6 +2,7 @@ import App from "../App";
 import renderer from 'react-test-renderer';
 import {fireEvent, render, screen} from '@testing-library/react';
 import {EinkaufHeader} from "../header/einkaufHeader";
+
 const setup = () => {
     const utils = render(<EinkaufHeader/>)
     const input = utils.getByPlaceholderText('Einkaufspunkt')
@@ -16,13 +17,4 @@ test("Submit", ()=>{
     const {input} = setup()
     fireEvent.change(input, {target: {value: 'kaufen2'}})
     expect(input.value).toEqual('kaufen2')
-
-   /* const component = renderer.create(
-        <App/>
-    ); */
-
-    //const element = screen.getByAltText()
-    //Eingabe , Submit, in der liste (data) vorhanden,
-
 })
-
