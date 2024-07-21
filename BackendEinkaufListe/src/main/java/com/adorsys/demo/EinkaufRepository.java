e85b445017a2bb71c6bd644563aea219410d927d
GeorgWenderoth <108282544+GeorgWenderoth@users.noreply.github.com>
2024-07-21 17:38:35 +0200

    Cleaned EinkaufRepository.java

--- BackendEinkaufListe/src/main/java/com/adorsys/demo/EinkaufRepository.java
+++ BackendEinkaufListe/src/main/java/com/adorsys/demo/EinkaufRepository.java
@@ -1,18 +1,14 @@
 package com.adorsys.demo;
 
-
 import com.adorsys.demo.EinkaufElement;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 import java.util.Optional;
 
-
 @Repository
-public interface EinkaufRepository extends JpaRepository<EinkaufElement, Long> { //Long
+public interface EinkaufRepository extends JpaRepository<EinkaufElement, Long> { 
         Optional<EinkaufElement> findByItId(int id); // findByItId, es muss genauso heißen wie der Propertiename, also findByElementID geht nicht, No Property Found for Type
         List<EinkaufElement> findAllByStrich(boolean strich);
         void deleteEinkaufElementsByStrichIsTrue();
-
 }
